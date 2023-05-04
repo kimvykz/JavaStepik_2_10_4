57fa1fe54913c952d7a397a99101a5ceeb404179
kimvykz <kimslavakz@gmail.com>
2023-05-04 18:04:47 +0600

    edited writing in output file with full expression

--- Program.java
+++ Program.java
@@ -1,5 +1,6 @@
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Program {
@@ -8,41 +9,85 @@ public class Program {
         double a, b, res_;
         String oper, as, bs;
 
-        try (FileReader fr = new FileReader("input.txt");
-             FileWriter fw = new FileWriter("output.txt")) {
-            Scanner sc = new Scanner(fr);
+        try {
+            FileReader fr = new FileReader("input.txt");
+            FileWriter fw = new FileWriter("output.txt", false);
 
-            as = sc.next() ;
-            oper = sc.next();
-            bs = sc.next();
+            int c;
+            String sTemp;
+            ArrayList<String> expressions = new ArrayList<>();
+            while ((c = fr.read()) != -1) {
+                sTemp = "" + (char) c;
 
-            a = Double.parseDouble(as);
-            b = Double.parseDouble(bs);
-            if (!"-+*/".contains(oper)) {
-                throw new UnsupportedOperationException("Operation Error!");
+                while ((c = fr.read()) != '\n' && c != -1) {
+                    sTemp += (char)c;
+                }
+                expressions.add(sTemp);
             }
-            if (oper.equals("/") && b == 0.0) {
-                throw new ArithmeticException("Error! Division by zero");
+            fr.close();
+            for (String exp : expressions) {
+                sTemp = exp + " = ";
+                try {
+
+                    Scanner sc = new Scanner(exp);
+
+                    as = sc.next();
+                    oper = sc.next();
+                    bs = sc.next();
+
+                    a = Double.parseDouble(as);
+                    b = Double.parseDouble(bs);
+                    if (!"-+*/".contains(oper)) {
+                        sTemp += "Operation Error!";
+                        throw new UnsupportedOperationException("Operation Error!");
+                    }
+                    if (oper.equals("/") && b == 0.0) {
+
+                        throw new ArithmeticException("Error! Division by zero");
+
+                    }
+
+                    res_ = calc(a, b, oper);
+                    sTemp += res_;
+                    fw.append(sTemp + "\n");
+                    fw.flush();
+                    System.out.println(sTemp);
+
+                }
+
+                catch (NumberFormatException nfEx) {
+                    sTemp += "Error! Not number";
+                    fw.append(sTemp + "\n");
+                    fw.flush();
+                    System.out.println("Error! Not number");
+
+
+                }
+                catch (UnsupportedOperationException uoEx) {
+                    sTemp += "Operation Error!";
+                    fw.append(sTemp + "\n");
+                    fw.flush();
+                    System.out.println("Operation Error!");
+                }
+                catch (ArithmeticException aEx) {
+                    sTemp += "Error! Division by zero";
+                    fw.append(sTemp + "\n");
+                    fw.flush();
+                    System.out.println("Error! Division by zero");
+                }
+
+
             }
 
-            res_ = calc(a, b, oper);
-            System.out.println(res_);
-
-            fw.write(String.valueOf(res_));
+            fw.close();
 
         }
-        catch (NumberFormatException nfEx) {
-            System.out.println("Error! Not number");
-        }
-        catch (UnsupportedOperationException uoEx) {
-            System.out.println("Operation Error!");
-        }
-        catch (ArithmeticException aEx) {
-            System.out.println("Error! Division by zero");
-        }
+
         catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
+
+
 
     }
 
